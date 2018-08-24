686ec2287fa79860110f5423d3d61c4dfecf186f
Jonte <highfestiva@gmail.com>
2018-08-24 16:30:25 +0200

    Add mouse click inspection callback
    Mouse click inspection callback is called with time and y-coordinate.
    Fixed crosshair and bar alignment (off-by-one).

--- finplot.py
+++ finplot.py
@@ -12,7 +12,7 @@ region.
 
 from datetime import datetime
 from functools import partial
-from math import log10, floor
+from math import log10, floor, fmod
 import numpy as np
 import pandas as pd
 import pyqtgraph as pg
@@ -24,13 +24,12 @@ legend_fill_color   = '#00000088'
 legend_text_color   = '#dddddd66'
 odd_plot_background = '#f0f0f0'
 band_color = '#aabbdd'
-cross_hair_color = '#555555aa'
+cross_hair_color = '#000000aa'
 significant_digits = 8
 
 prev_ax = None
 pre_win = None
-
-_epoch2local = lambda t: datetime.fromtimestamp(t).isoformat().replace('T',' ').rsplit(':',1)[0]
+epoch_period2 = 0.5
 
 
 
@@ -54,6 +53,8 @@ class PandasDataSource:
         self.skip_scale_colcnt = 1 # skip at least time for hi/lo, for candle sticks+volume we also skip open and close
         self.cache_hilo_query = ''
         self.cache_hilo_answer = None
+        global epoch_period2
+        epoch_period2 = self.period / 2
 
     @property
     def period(self):
@@ -70,8 +71,12 @@ class PandasDataSource:
         ycol = self.df.columns[1]
         return self.df[ycol]
 
-    def addcols(self, df):
-        newcols = df[df.columns[1:]] # skip timecol
+    def closest_time(self, t):
+        t0,_,_,_,_ = self._hilo(t, t+self.period)
+        return t0
+
+    def addcols(self, datasrc):
+        newcols = datasrc.df[datasrc.df.columns[1:]] # skip timecol
         self.df = pd.concat([self.df, newcols], axis=1)
 
     def get_time(self, offset_from_end=0, period=0):
@@ -135,14 +140,14 @@ class FinCrossHair:
         ax.addItem(self.xtext, ignoreBounds=True)
         ax.addItem(self.ytext, ignoreBounds=True)
 
-    def update(self, pos):
-        point = self.ax.vb.mapSceneToView(pos)
-        self.vline.setPos(point.x())
+    def update(self, point):
+        x = point.x() - fmod(point.x()-epoch_period2, epoch_period2*2) + epoch_period2
+        self.vline.setPos(x)
         self.hline.setPos(point.y())
         self.xtext.setPos(point)
         self.ytext.setPos(point)
-        space = '          '
-        self.xtext.setText(space + _epoch2local(point.x()))
+        space = '      '
+        self.xtext.setText(space + _epoch2local(x))
         value = _round_to_significant(point.y(), significant_digits)
         self.ytext.setText(space + value)
 
@@ -195,9 +200,9 @@ class FinViewBox(pg.ViewBox):
         scale_fact = 1.02 ** (ev.delta() * self.state['wheelScaleFactor'])
         vr = self.targetRect()
         center = pg.Point(pg.functions.invertQTransform(self.childGroup.transform()).map(ev.pos()))
-        if (center.x()-vr.left())/vr.width() < 0.2: # zoom to far left => all the way left
+        if (center.x()-vr.left())/vr.width() < 0.05: # zoom to far left => all the way left
             center = pg.Point(vr.left(), center.y())
-        elif (center.x()-vr.left())/vr.width() > 0.8: # zoom to far right => all the way right
+        elif (center.x()-vr.left())/vr.width() > 0.95: # zoom to far right => all the way right
             center = pg.Point(vr.right(), center.y())
         self.scaleRect(vr, scale_fact, center)
         ev.accept()
@@ -266,17 +271,17 @@ class CandlestickItem(FinPlotItem):
         self.picture = QtGui.QPicture()
         p = QtGui.QPainter(self.picture)
         w = self.datasrc.period * 0.7
-        w2 = w / 2
+        w2 = w * 0.5
         p.setPen(pg.mkPen(self.bear_color))
         p.setBrush(pg.mkBrush(self.bear_color))
         for t,open,close,high,low in self.datasrc.bear_rows():
-            p.drawLine(QtCore.QPointF(t+w2, low), QtCore.QPointF(t+w2, high))
-            p.drawRect(QtCore.QRectF(t, open, w, close-open))
+            p.drawLine(QtCore.QPointF(t, low), QtCore.QPointF(t, high))
+            p.drawRect(QtCore.QRectF(t-w2, open, w, close-open))
         p.setPen(pg.mkPen(self.bull_color))
         p.setBrush(pg.mkBrush(self.bull_color))
         for t,open,close,high,low in self.datasrc.bull_rows():
-            p.drawLine(QtCore.QPointF(t+w2, low), QtCore.QPointF(t+w2, high))
-            p.drawRect(QtCore.QRectF(t, open, w, close-open))
+            p.drawLine(QtCore.QPointF(t, low), QtCore.QPointF(t, high))
+            p.drawRect(QtCore.QRectF(t-w2, open, w, close-open))
         p.end()
 
 
@@ -289,14 +294,15 @@ class VolumeItem(FinPlotItem):
         self.picture = QtGui.QPicture()
         p = QtGui.QPainter(self.picture)
         w = self.datasrc.period * 0.7
+        w2 = w * 0.5
         p.setPen(pg.mkPen(self.bear_color))
         p.setBrush(pg.mkBrush(self.bear_color))
         for t,open,close,volume in self.datasrc.bear_rows():
-            p.drawRect(QtCore.QRectF(t, 0, w, volume))
+            p.drawRect(QtCore.QRectF(t-w2, 0, w, volume))
         p.setPen(pg.mkPen(self.bull_color))
         p.setBrush(pg.mkBrush(self.bull_color))
         for t,open,close,volume in self.datasrc.bull_rows():
-            p.drawRect(QtCore.QRectF(t, 0, w, volume))
+            p.drawRect(QtCore.QRectF(t-w2, 0, w, volume))
         p.end()
 
 
@@ -311,12 +317,85 @@ def create_plot(title=None, rows=1, init_zoom_periods=300, maximize=True):
     axs = []
     for n in range(rows):
         viewbox = FinViewBox(win, init_steps=init_zoom_periods)
-        axs += [add_timestamp_plot(win, viewbox, n)]
-    win.proxy = pg.SignalProxy(win.scene().sigMouseMoved, rateLimit=60, slot=partial(_mouse_moved, win))
+        axs += [_add_timestamp_plot(win, viewbox, n)]
+    win.proxy_mmove = pg.SignalProxy(win.scene().sigMouseMoved, rateLimit=60, slot=partial(_mouse_moved, win))
     return axs
 
 
-def add_timestamp_plot(win, viewbox, n):
+def candlestick_ochl(datasrc, bull_color='#44bb55', bear_color='#dd6666', ax=None):
+    if ax is None:
+        ax = create_plot(maximize=False)
+    datasrc.skip_scale_colcnt = 3 # skip open+close for scaling
+    _update_datasrc(ax, datasrc)
+    item = CandlestickItem(datasrc=datasrc, bull_color=bull_color, bear_color=bear_color)
+    ax.addItem(item)
+    ax.vb.add_heavy_item(item) # heavy = deferred rendering
+    _update_main_plot(ax)
+    return item
+
+
+def volume_ocv(datasrc, bull_color='#44bb55', bear_color='#dd6666', ax=None):
+    if ax is None:
+        ax = create_plot(maximize=False)
+    datasrc.skip_scale_colcnt = 3 # skip open+close for scaling
+    _update_datasrc(ax, datasrc)
+    item = VolumeItem(datasrc=datasrc, bull_color=bull_color, bear_color=bear_color)
+    ax.addItem(item)
+    ax.vb.add_heavy_item(item) # heavy = deferred rendering
+    _update_main_plot(ax)
+    return item
+
+
+def plot(x, y, color='#000000', ax=None, style=None, legend=None):
+    datasrc = PandasDataSource(pd.concat([x,y], axis=1))
+    return plot_datasrc(datasrc, color=color, ax=ax, style=style, legend=legend)
+
+
+def plot_datasrc(datasrc, color='#000000', ax=None, style=None, legend=None):
+    if ax is None:
+        ax = create_plot(maximize=False)
+    _update_datasrc(ax, datasrc)
+    if legend is not None and ax.legend is None:
+        ax.legend = FinLegendItem(border_color=legend_border_color, fill_color=legend_fill_color, size=None, offset=(3,2))
+        ax.legend.setParentItem(ax.vb)
+    if style is None or style=='-':
+        item = ax.plot(datasrc.x, datasrc.y, pen=pg.mkPen(color), name=legend)
+    else:
+        symbol = {'v':'t', '^':'t1', '>':'t2', '<':'t3'}.get(style, style) # translate some similar styles
+        item = ax.plot(datasrc.x, datasrc.y, pen=None, symbol=symbol, symbolPen=None, symbolSize=10, symbolBrush=pg.mkBrush(color), name=legend)
+    if ax.legend is not None:
+        for _,label in ax.legend.items:
+            label.setText(label.text, color=legend_text_color)
+    _update_main_plot(ax)
+    return item
+
+
+def set_y_range(ax, ymin, ymax):
+    ax.setLimits(yMin=ymin, yMax=ymax)
+
+
+def add_band(ax, y0, y1, color=band_color):
+    ax.vb.setBackgroundColor(None)
+    lr = pg.LinearRegionItem([y0,y1], orientation=pg.LinearRegionItem.Horizontal, brush=pg.mkBrush(color), movable=False)
+    lr.setZValue(-10)
+    ax.addItem(lr)
+
+
+def add_time_inspector(ax, inspector):
+    win = ax.vb.win
+    win.proxy_click = pg.SignalProxy(win.scene().sigMouseClicked, slot=partial(_time_clicked, ax, inspector))
+
+
+def show():
+    QtGui.QApplication.instance().exec_()
+
+
+
+
+#################### INTERNALS ####################
+
+
+def _add_timestamp_plot(win, viewbox, n):
     global prev_ax
     if prev_ax is not None:
         prev_ax.hideAxis('bottom') # hide the whole previous axis
@@ -333,31 +412,20 @@ def add_timestamp_plot(win, viewbox, n):
     return ax
 
 
-def set_y_range(ax, ymin, ymax):
-    ax.setLimits(yMin=ymin, yMax=ymax)
-
-
-def add_band(ax, y0, y1, color=band_color):
-    ax.vb.setBackgroundColor(None)
-    lr = pg.LinearRegionItem([y0,y1], orientation=pg.LinearRegionItem.Horizontal, brush=pg.mkBrush(color), movable=False)
-    lr.setZValue(-10)
-    ax.addItem(lr)
-
-
-def update_datasrc(ax, datasrc):
+def _update_datasrc(ax, datasrc):
     viewbox = ax.vb
     if viewbox.datasrc is None:
         viewbox.set_datasrc(datasrc) # for mwheel zoom-scaling
-        x0 = datasrc.get_time(1e20, period=-0.5)
-        x1 = datasrc.get_time(0, period=+1.0)
+        x0 = datasrc.get_time(1e20, period=-1.0)
+        x1 = datasrc.get_time(0, period=+0.5)
         ax.setLimits(xMin=x0, xMax=x1)
     else:
-        viewbox.datasrc.addcols(datasrc.df)
+        viewbox.datasrc.addcols(datasrc)
         viewbox.datasrc.skip_scale_colcnt = max(viewbox.datasrc.skip_scale_colcnt, datasrc.skip_scale_colcnt)
         viewbox.set_datasrc(viewbox.datasrc) # update zoom
 
 
-def update_main_plot(ax):
+def _update_main_plot(ax):
     '''The first plot to add some data is the leader. All other's X-axis will follow this one.'''
     if ax.vb.linkedView(0):
         return
@@ -366,68 +434,29 @@ def update_main_plot(ax):
             ax_.setXLink(ax.vb.name)
 
 
-def candlestick_ochl(datasrc, bull_color='#44bb55', bear_color='#dd6666', ax=None):
-    if ax is None:
-        ax = create_plot(maximize=False)
-    datasrc.skip_scale_colcnt = 3 # skip open+close for scaling
-    update_datasrc(ax, datasrc)
-    item = CandlestickItem(datasrc=datasrc, bull_color=bull_color, bear_color=bear_color)
-    ax.addItem(item)
-    ax.vb.add_heavy_item(item) # heavy = deferred rendering
-    update_main_plot(ax)
-    return item
-
-
-def volume_ocv(datasrc, bull_color='#44bb55', bear_color='#dd6666', ax=None):
-    if ax is None:
-        ax = create_plot(maximize=False)
-    datasrc.skip_scale_colcnt = 3 # skip open+close for scaling
-    update_datasrc(ax, datasrc)
-    item = VolumeItem(datasrc=datasrc, bull_color=bull_color, bear_color=bear_color)
-    ax.addItem(item)
-    ax.vb.add_heavy_item(item) # heavy = deferred rendering
-    update_main_plot(ax)
-    return item
-
-
-def plot(x, y, color='#000000', ax=None, style=None, legend=None):
-    datasrc = PandasDataSource(pd.concat([x,y], axis=1))
-    return plot_datasrc(datasrc, color=color, ax=ax, style=style, legend=legend)
-
-
-def plot_datasrc(datasrc, color='#000000', ax=None, style=None, legend=None):
-    if ax is None:
-        ax = create_plot(maximize=False)
-    update_datasrc(ax, datasrc)
-    if legend is not None and ax.legend is None:
-        ax.legend = FinLegendItem(border_color=legend_border_color, fill_color=legend_fill_color, size=None, offset=(3,2))
-        ax.legend.setParentItem(ax.vb)
-    if style is None or style=='-':
-        item = ax.plot(datasrc.x, datasrc.y, pen=pg.mkPen(color), name=legend)
-    else:
-        symbol = {'v':'t', '^':'t1', '>':'t2', '<':'t3'}.get(style, style) # translate some similar styles
-        item = ax.plot(datasrc.x, datasrc.y, pen=None, symbol=symbol, symbolPen=None, symbolSize=10, symbolBrush=pg.mkBrush(color), name=legend)
-    if ax.legend is not None:
-        for _,label in ax.legend.items:
-            label.setText(label.text, color=legend_text_color)
-    update_main_plot(ax)
-    return item
-
-
-def show():
-    QtGui.QApplication.instance().exec_()
-
-
 def _mouse_moved(win, ev):
     pos = ev[0]
     for ax in win.ci.items:
-        ax.crosshair.update(pos)
+        point = ax.vb.mapSceneToView(pos)
+        ax.crosshair.update(point)
+
+
+def _time_clicked(ax, inspector, ev):
+    pos = ev[0].scenePos()
+    point = ax.vb.mapSceneToView(pos)
+    t = point.x() - epoch_period2
+    t = ax.vb.datasrc.closest_time(t)
+    inspector(t, point.y())
 
 
 def _pdtime2epoch(t):
     if type(t) is pd.Series and type(t.iloc[0]) is pd.Timestamp:
         return t.astype('int64') // int(1e9)
     return t
+
+
+def _epoch2local(t):
+    return datetime.fromtimestamp(t).isoformat().replace('T',' ').rsplit(':',1)[0]
 
 
 def _round_to_significant(x, num_significant):
