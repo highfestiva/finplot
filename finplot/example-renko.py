cbb87e24c799e4e994b74b696c0b143cf9940a98
Jonte <highfestiva@gmail.com>
2020-08-29 07:32:30 +0200

    Oops: remove accidental modifications to example

--- finplot/example-renko.py
+++ finplot/example-renko.py
@@ -3,8 +3,7 @@
 import finplot as fplt
 import yfinance as yf
 
-df = yf.download('BTC-USD', '2014-01-01')
-import numpy as np
-df['c'] = df.Close#np.log(df.Close)
-fplt.renko(df.c, bins=100)
+df = yf.download('BNO', '2014-01-01')
+fplt.create_plot('US Brent Oil Renko', maximize=False)
+fplt.renko(df.Close)
 fplt.show()
