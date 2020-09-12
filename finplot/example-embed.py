ba75cbba8f915d2a7a2c85b6eec35cc5343370dc
Jonte <highfestiva@gmail.com>
2020-09-12 12:43:19 +0200

    Extend embedding example with showing asset name

--- finplot/example-embed.py
+++ finplot/example-embed.py
@@ -2,8 +2,9 @@
 
 import finplot as fplt
 from functools import lru_cache
-from PyQt5.QtWidgets import QGraphicsView, QComboBox
+from PyQt5.QtWidgets import QGraphicsView, QComboBox, QLabel
 from PyQt5.QtGui import QApplication, QGridLayout
+from threading import Thread
 import yfinance as yf
 
 
@@ -16,30 +17,30 @@ win.resize(600, 500)
 
 combo = QComboBox()
 combo.setEditable(True)
-combo.addItem('SPY')
-combo.addItem('BNO')
-combo.addItem('CT=F')
-combo.addItem('AAPL')
-combo.addItem('WMT')
-combo.addItem('GOOG')
-combo.addItem('BTC-USD')
-combo.addItem('ETH-USD')
+[combo.addItem(i) for i in 'AMRK FB GFN REVG TWTR WMT CT=F GC=F ^FTSE ^N225 EURUSD=X ETH-USD'.split()]
 layout.addWidget(combo, 0, 0, 1, 1)
+info = QLabel()
+layout.addWidget(info, 0, 1, 1, 1)
 
-ax = fplt.create_plot_widget(win)
+ax = fplt.create_plot_widget(win, init_zoom_periods=100)
 win.axs = [ax] # finplot requres this property
-layout.addWidget(ax.ax_widget, 1, 0, 1, 1)
+layout.addWidget(ax.ax_widget, 1, 0, 1, 2)
 
 
 @lru_cache(maxsize=15)
 def download(symbol):
     return yf.download(symbol, '2019-01-01')
 
+@lru_cache(maxsize=100)
+def get_name(symbol):
+    return yf.Ticker(symbol).info['shortName']
+
 plots = []
 def update(txt):
     df = download(txt)
     if len(df) < 20: # symbol does not exist
         return
+    info.setText('Loading symbol name...')
     price = df['Open Close High Low'.split()]
     ma20 = df.Close.rolling(20).mean()
     ma50 = df.Close.rolling(50).mean()
@@ -54,6 +55,8 @@ def update(txt):
         plots[1].update_data(ma20)
         plots[2].update_data(ma50)
         plots[3].update_data(volume)
+    Thread(target=lambda: info.setText(get_name(txt))).start() # slow, so use thread
+
 combo.currentTextChanged.connect(update)
 update(combo.currentText())
 
