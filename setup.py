db69d174e988fb6e9f616d33711d91ded01340fa
Jonte <highfestiva@gmail.com>
2021-01-02 00:23:12 +0100

    Patch release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='finplot',
-    version='1.5.0',
+    version='1.5.1',
     author='Jonas Byström',
     author_email='highfestiva@gmail.com',
     description='Finance plotting',
