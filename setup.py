fd83f64e23b15b2557846205566cc53762247b57
Jonte <highfestiva@gmail.com>
2021-01-05 11:33:16 +0100

    Make API parameter name change public

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='finplot',
-    version='1.5.2',
+    version='1.5.3',
     author='Jonas Byström',
     author_email='highfestiva@gmail.com',
     description='Finance plotting',
