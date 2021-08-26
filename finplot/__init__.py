10894c34caaa5df94bb494e91355f7f12ccb9f19
Jonte <highfestiva@gmail.com>
2021-08-26 20:46:11 +0200

    Keep decimals consistent for all values on Y-axis

--- finplot/__init__.py
+++ finplot/__init__.py
@@ -130,11 +130,26 @@ class YAxisItem(pg.AxisItem):
         self.hide_strings = False
         self.style['autoExpandTextSpace'] = False
         self.style['autoReduceTextSpace'] = False
+        self.next_fmt = '%g'
+
+    def tickValues(self, minVal, maxVal, size):
+        vs = super().tickValues(minVal, maxVal, size)
+        if len(vs) < 3:
+            return vs
+        xform = self.vb.yscale.xform
+        gs = ['%g'%xform(v) for v in vs[2][1]]
+        maxdec = max([len((g).partition('.')[2].partition('e')[0]) for g in gs])
+        if any(['e' in g for g in gs]):
+            self.next_fmt = '%%.%ig' % maxdec
+        else:
+            self.next_fmt = '%%.%if' % maxdec
+        return vs
 
     def tickStrings(self, values, scale, spacing):
         if self.hide_strings:
             return []
-        return ['%g'%self.vb.yscale.xform(value) for value in values]
+        xform = self.vb.yscale.xform
+        return [self.next_fmt%xform(value) for value in values]
 
 
 
