916f45d19957fb569717a25d12f8692223ae114e
Jonte <highfestiva@gmail.com>
2022-03-09 18:39:44 +0100

    Release v1.8.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='finplot',
-    version='1.8',
+    version='1.8.1',
     author='Jonas Byström',
     author_email='highfestiva@gmail.com',
     description='Finance plotting',
