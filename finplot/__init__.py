b1f600996dcdb9c44a7913eb928a2ff963c4b48a
Jonte <highfestiva@gmail.com>
2022-05-11 07:36:34 +0200

    Improve log Y-axis ticks

--- finplot/__init__.py
+++ finplot/__init__.py
@@ -176,20 +176,36 @@ class YAxisItem(pg.AxisItem):
         vs = super().tickValues(minVal, maxVal, size)
         if len(vs) < 3:
             return vs
-        xform = self.vb.yscale.xform
-        gs = ['%g'%xform(v) for v in vs[2][1]]
-        maxdec = max([len((g).partition('.')[2].partition('e')[0]) for g in gs])
-        if any(['e' in g for g in gs]):
-            self.next_fmt = '%%.%ie' % maxdec
-        else:
-            self.next_fmt = '%%.%if' % maxdec
-        return vs
+        return self.fmt_values(vs)
+
+    def logTickValues(self, minVal, maxVal, size, stdTicks):
+        v1 = int(floor(minVal))
+        v2 = int(ceil(maxVal))
+        minor = []
+        for v in range(v1, v2):
+            minor.extend([v+l for l in np.log10(np.linspace(1, 9.9, 90))])
+        minor = [x for x in minor if x>minVal and x<maxVal]
+        if len(minor) > 10:
+            minor = minor[::len(minor)//5]
+        vs = [(None, minor)]
+        return self.fmt_values(vs)
 
     def tickStrings(self, values, scale, spacing):
         if self.hide_strings:
             return []
         xform = self.vb.yscale.xform
         return [self.next_fmt%xform(value) for value in values]
+
+    def fmt_values(self, vs):
+        xform = self.vb.yscale.xform
+        gs = ['%g'%xform(v) for v in vs[-1][1]]
+        if any(['e' in g for g in gs]):
+            maxdec = max([len((g).partition('.')[2].partition('e')[0]) for g in gs if 'e' in g])
+            self.next_fmt = '%%.%ie' % maxdec
+        else:
+            maxdec = max([len((g).partition('.')[2]) for g in gs])
+            self.next_fmt = '%%.%if' % maxdec
+        return vs
 
 
 
