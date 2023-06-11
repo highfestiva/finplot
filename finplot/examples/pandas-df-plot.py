f77ccfe833ec95795ca21dce231b4d2ec0c90c52
Jonte <highfestiva@gmail.com>
2023-06-12 01:22:08 +0200

    Add labels() call with html styling

--- finplot/examples/pandas-df-plot.py
+++ finplot/examples/pandas-df-plot.py
@@ -3,10 +3,11 @@
 import finplot as fplt
 import pandas as pd
 
-df = pd.DataFrame({'A':[1,2,3,5], 'B':[2,4,5,3], 'C':[3,6,11,8], 'D':[1,1,2,2], 'labels':list('ABCD')}, index=[1606086000, 1606086001, 1606086002, 1606086003])
+labels = ['A', 'B', '<span style="text-decoration:underline; color:#00b">C</span>', '<span style="font-weight:bold">D</span>']
+df = pd.DataFrame({'A':[1,2,3,5], 'B':[2,4,5,3], 'C':[3,6,11,8], 'D':[1,1,2,2], 'labels':labels}, index=[1606086000, 1606086001, 1606086002, 1606086003])
 
 df.plot('A')
-df.plot('A', 'labels', kind='labels', color='#660')
+df.plot('A', 'labels', kind='labels', color='#b90')
 df.plot('C', kind='scatter')
 (1-df.B).plot(kind='scatter')
 df.plot(kind='candle').setZValue(-100)
